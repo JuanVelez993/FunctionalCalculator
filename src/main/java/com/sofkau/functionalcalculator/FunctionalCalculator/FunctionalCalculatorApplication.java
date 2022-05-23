e539e79d0e64a1528dc496a4af23c3847de5a210
Juan Pablo Velez <juan.velez993@gmail.com>
2022-05-23 16:52:05 -0500

    ADD---sum and subtraction to main class

--- src/main/java/com/sofkau/functionalcalculator/FunctionalCalculator/FunctionalCalculatorApplication.java
+++ src/main/java/com/sofkau/functionalcalculator/FunctionalCalculator/FunctionalCalculatorApplication.java
@@ -1,13 +1,29 @@
 package com.sofkau.functionalcalculator.FunctionalCalculator;
 
+import com.sofkau.functionalcalculator.FunctionalCalculator.interfaces.MultiplicationInterface;
+import com.sofkau.functionalcalculator.FunctionalCalculator.interfaces.SubtractionInterface;
+import com.sofkau.functionalcalculator.FunctionalCalculator.interfaces.SumInterface;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 public class FunctionalCalculatorApplication {
 
+	static SumInterface isum = (n1, n2) -> n1 + n2;
+
+	static SubtractionInterface isubstract = (n1, n2) -> n1 - n2;
+	static MultiplicationInterface imultiplication=(n1, n2) -> n1 - n2;
+
 	public static void main(String[] args) {
-		SpringApplication.run(FunctionalCalculatorApplication.class, args);
+		//sum
+		System.out.println("Sum of two numbers: "+isum.sum(3,5));
+
+		//substraction
+		System.out.println("Substraction of two numbers: "+isubstract.subtraction(10,5));
+
+		//multiplication
+
+		//division
 	}
 
 }
